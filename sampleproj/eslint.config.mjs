5502fce669c6c4951117fd4601690a81679c05d7
Dhivya-Relevantz <dhivya.kannan@relevantz.com>
2025-09-08 17:01:08 +0530

    eslint Update

--- sampleproj/eslint.config.mjs
+++ sampleproj/eslint.config.mjs
@@ -1,15 +1,16 @@
+import path from 'path';
 import powerbiVisualsConfigs from "eslint-plugin-powerbi-visuals";
 
 export default [
-    {
-        ...powerbiVisualsConfigs.configs.recommended,
-        parserOptions: {
-            ...powerbiVisualsConfigs.configs.recommended.parserOptions,
-            tsconfigRootDir: __dirname,
-            project: './tsconfig.json'
-        }
-    },
-    {
-        ignores: ["node_modules/**", "dist/**", ".vscode/**", ".tmp/**"],
-    },
+  {
+    ...powerbiVisualsConfigs.configs.recommended,
+    parserOptions: {
+      ...powerbiVisualsConfigs.configs.recommended.parserOptions,
+      tsconfigRootDir: path.resolve(__dirname),
+      project: './tsconfig.json'
+    }
+  },
+  {
+    ignores: ["node_modules/**", "dist/**", ".vscode/**", ".tmp/**"]
+  }
 ];
